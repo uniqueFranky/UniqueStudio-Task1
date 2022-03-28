190e8277acb1f51b069d5dbc51b800cf5b818862
uniqueFranky <y_r_b@qq.com>
2022-03-28 19:35:06 +0800

    Add Raw CollectionView

---
+++ UIKitPractice/CollectionViewCell.swift
@@ -0,0 +1,59 @@
+//
+//  CollectionViewCell.swift
+//  UIKitPractice
+//
+//  Created by 闫润邦 on 2022/3/28.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(imageView)
+        addSubview(label)
+        configureConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("No!")
+    }
+    
+    var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage()
+//        imageView.sizeToFit()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    
+    var label: UILabel = {
+        let label = UILabel()
+        label.text = "Hello World!"
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.adjustsFontForContentSizeCategory = true
+        label.textColor = .black
+        return label
+    }()
+
+    
+    func configureConstraints() {
+
+        
+        let constraints = [
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50),
+            imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 5),
+            label.centerXAnchor.constraint(equalTo: imageView.centerXAnchor),
+        ]
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        addConstraints(constraints)
+
+    }
+}
---
+++ UIKitPractice/CollectionViewController.swift
@@ -0,0 +1,67 @@
+//
+//  CollectionViewController.swift
+//  UIKitPractice
+//
+//  Created by 闫润邦 on 2022/3/28.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class CollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(CollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        // Do any additional setup after loading the view.
+        collectionView.backgroundColor = .white
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        
+        configureConstraints()
+    }
+    
+    func configureConstraints() {
+        let constraints = [
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 250),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -250),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+        ]
+        view.addConstraints(constraints)
+    }
+
+    // MARK: UICollectionViewDataSource
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CollectionViewCell
+        
+        // Configure the cell
+        cell.imageView.image = UIImage(systemName: "airplane")
+        cell.layer.borderColor = CGColor(red: 0, green: 0, blue: 255, alpha: 1)
+        cell.layer.borderWidth = 2
+        cell.layer.cornerRadius = 30
+        cell.label.text = "Hello World!\(indexPath.item)"
+        return cell
+    }
+
+
+}
+
+extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: 300, height: 300)
+    }
+}
