9ca61ecceeea0c965bf9340b7048f15ff2711086
uniqueFranky <y_r_b@qq.com>
2022-03-30 23:37:23 +0800

    (feature): Add Video

--- UIKitPractice/Champion.swift
+++ UIKitPractice/Champion.swift
@@ -19,4 +19,5 @@ class Skill: Codable, Identifiable {
     var id: String
     var name: String
     var description: String
+    var videoable: Bool
 }
--- UIKitPractice/DetailViewController.swift
+++ UIKitPractice/DetailViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class DetailViewController: UIViewController {
     var championName = ""
     var skillName = ""
     var skillId = ""
+    var videoable = false
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -25,6 +28,17 @@ class DetailViewController: UIViewController {
         skillImageView.image = UIImage(named: "\(championName.lowercased())_\(skillId.lowercased()).png")
         skillImageView.contentMode = .scaleAspectFill
         view.addSubview(skillImageView)
+        
+        let button = UIButton()
+        button.setTitle("Tap to See the Video", for: .normal)
+        button.backgroundColor = .darkGray
+        if !videoable {
+            button.setTitle("The Skill Cannot Be Displayed as A Video", for: .normal)
+            button.backgroundColor = .black
+            button.isEnabled = false
+        }
+        button.addTarget(self, action: #selector(loadMovie), for: .touchUpInside)
+        view.addSubview(button)
         
         let constraints = [
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -41,10 +55,18 @@ class DetailViewController: UIViewController {
             skillImageView.bottomAnchor.constraint(equalTo: championImageView.bottomAnchor),
             skillImageView.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: 25),
             skillImageView.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: 75),
+            
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 30),
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            button.bottomAnchor.constraint(equalTo: button.topAnchor, constant: 50)
         ]
+        
         label.translatesAutoresizingMaskIntoConstraints = false
         championImageView.translatesAutoresizingMaskIntoConstraints = false
         skillImageView.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
         view.addConstraints(constraints)
     }
     
@@ -55,5 +77,27 @@ class DetailViewController: UIViewController {
         label.numberOfLines = 0
         return label
     }()
-   
+    let playerViewController = AVPlayerViewController()
+
+    @objc func loadMovie() {
+        guard let videoURL = Bundle.main.url(forResource: "\(championName.lowercased())_\(skillId.lowercased()).mp4", withExtension: nil)
+            else {
+                fatalError("Couldn't find video in main bundle.")
+        }
+        let player = AVPlayer(url: videoURL)
+        playerViewController.player = player
+
+        self.present(playerViewController, animated: true) { [self] in
+            playerViewController.player!.play()
+            NotificationCenter.default.addObserver(self,
+                                                   selector: #selector(self.playerItemDidReachEnd),
+                                                             name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+                                                             object: nil) // Add observer
+        }
+    }
+    
+    @objc func playerItemDidReachEnd() {
+        playerViewController.dismiss(animated: true, completion: nil)
+    }
+    
 }
